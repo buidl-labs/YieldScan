20a12d674c1e0504df5cf72476f3e1dbdc5a5842
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2019-12-22 15:01:19 +0530

    load dashboard from 15sec to within 3secs approx
    earlier validators data was being fetched by making multiple requests and all the computation was done on client side, now it has been moved to server & only one request is being made to improve load time even further

--- polka-analytics-app/src/App.jsx
+++ polka-analytics-app/src/App.jsx
@@ -23,7 +23,6 @@ import {
 } from "@chakra-ui/core";
 import { useDebounce } from "use-debounce";
 import { ApiPromise, WsProvider } from "@polkadot/api";
-import { hexToString } from "@polkadot/util";
 import ValidatorTable from "./components/ValidatorTable";
 import HelpCenter from "./components/HelpCenter";
 import amplitude from "amplitude-js";
@@ -31,6 +30,7 @@ import { AmplitudeProvider, LogOnMount } from "@amplitude/react-amplitude";
 import ScrollToTop from "./ScrollToTop";
 import ValidatorApp from "./components/validator_components/ValidatorApp";
 import NominatorApp from "./components/nominator_components/NominatorApp";
+import socketIOClient from "socket.io-client";
 
 const AMPLITUDE_KEY = "1d3873d97d87e9193e7e30529d8a10ab";
 
@@ -49,123 +49,6 @@ function App() {
 	const [apiConnected, setApiConnected] = React.useState(false);
 	const [isLoaded, setIsLoaded] = React.useState(false);
 	const ERA_PER_DAY = 4;
-
-	const createApi = async () => {
-		// console.log(`Connecting to API...`);
-		const wsProvider = new WsProvider("wss://kusama-rpc.polkadot.io");
-		const api = await ApiPromise.create({ provider: wsProvider });
-		await api.isReady;
-		// console.log(`API is ready`);
-		// console.clear();
-		// Fetch recent reward events from Polkascan
-		const res = await fetch(
-			"https://polkascan.io/kusama-cc3/api/v1/event?&filter[module_id]=staking&filter[event_id]=Reward&page[size]=10"
-		);
-		const json = await res.json();
-		const rewardData = await json.data;
-
-		// Retrieve currentElected validators for current block
-		const currentValidators = await api.query.staking.currentElected();
-		// Retrieve all validators
-		// const allValidators = await api.query.staking.validators();
-		
-		// Parse validators
-		// const parsedValidators = JSON.parse(JSON.stringify(allValidators))[0];
-		// Retrieve session validators
-		
-		// const sessionValidators = await api.query.session.validators();
-		
-		// const intentions = await parsedValidators.filter(
-		// 	validator => !sessionValidators.includes(validator)
-		// );
-		// const validatorsAndIntentions = [...sessionValidators, ...intentions];
-		// Retrieve the last known era reward
-		const reward = await rewardData[0].attributes.attributes[0].value;
-		// Retrieve the hashes of the end of era blocks
-		const hash = await Promise.all(
-			rewardData.map(data =>
-				api.rpc.chain.getBlockHash(data.attributes.block_id - 1)
-			)
-		);
-		// Retrieve the era points for all end of era blocks
-		const eraPoints = await Promise.all(
-			hash.map(data =>
-				api.query.staking.currentEraPointsEarned.at(`${data.toString()}`)
-			)
-		);
-		// Retrieve an array of the list of all elected validators at the end of era blocks
-		const validatorList = await Promise.all(
-			hash.map(data =>
-				api.query.staking.currentElected.at(`${data.toString()}`)
-			)
-		);
-
-		let result = {};
-
-		await Promise.all(
-			validatorList.map(async validator => {
-				await Promise.all(
-					validator.map(async address => {
-						const commission = await api.query.staking.validators(address);
-						const name = await api.query.nicks.nameOf(`${address.toString()}`);
-						result[address] = {
-							stashId: address.toString(),
-							stashIdTruncated: `${address
-								.toString()
-								.slice(0, 4)}...${address.toString().slice(-6, -1)}`,
-							points: [],
-							poolReward: "",
-							totalStake: "",
-							commission: commission[0].commission.toNumber() / 10 ** 7,
-							name: name.raw[0]
-								? hexToString(name.raw[0].toString())
-								: `Validator (...${address.toString().slice(-6, -1)})`
-						};
-					})
-				);
-			})
-		);
-
-		eraPoints.map((eraPoint, index) => {
-			eraPoint.individual.map((point, validatorIndex) => {
-				result[validatorList[index][validatorIndex]].points.push(
-					point.toNumber() / eraPoint.total.toNumber()
-				);
-				return 0;
-			});
-			return 0;
-		});
-
-		const validatorData = await Promise.all(
-			Object.keys(result).map(async (key, index) => {
-				const validatorPoolReward =
-					((result[key].points.reduce((acc, curr) => acc + curr, 0) /
-						result[key].points.length) *
-						reward) /
-					10 ** 12;
-				const electedInfo = await api.derive.staking.electedInfo();
-				const stakeInfo = await api.derive.staking.account(key.toString());
-				const totalStake =
-					stakeInfo !== undefined
-						? stakeInfo.stakers.total.toString() / 10 ** 12
-						: undefined;
-				result[key].totalStake = totalStake;
-				result[key].poolReward = isNaN(validatorPoolReward)
-					? "Not enough data"
-					: (1 - result[key].commission / 100) * validatorPoolReward;
-				setElectedInfo(electedInfo);
-				return result[key];
-			})
-		);
-		const filteredValidatorData = validatorData.filter(curr =>
-			currentValidators.includes(curr.stashId)
-		);
-		setApiConnected(true);
-		setValidatorData(filteredValidatorData);
-		// setIntentionData(intentions);
-		// setValidatorsAndIntentions(validatorsAndIntentions);
-		return filteredValidatorData;
-	};
 
 	const calcReward = React.useCallback(() => {
 		const data = validatorData.map(validator => {
@@ -196,13 +79,33 @@ function App() {
 		if (apiConnected) setIsLoaded(true);
 	}, [stakeAmount, validatorData, apiConnected]);
 
+	const getElectedInfo = async() => {
+		const wsProvider = new WsProvider("wss://kusama-rpc.polkadot.io");
+		const api = await ApiPromise.create({ provider: wsProvider });
+		await api.isReady;
+		const electedInfo = await api.derive.staking.electedInfo();
+		setElectedInfo(electedInfo);
+	}
+
 	React.useEffect(() => {
-		createApi();
-	}, []);
+		getElectedInfo();
+	}, [])
 
 	React.useEffect(() => {
 		if (apiConnected) calcReward();
 	}, [calcReward, apiConnected]);
+
+	React.useEffect(() => {
+		const socket = socketIOClient("http://localhost:3004/");
+		socket.on("initial", (data) => {
+			setApiConnected(true);
+			setValidatorData(data);
+		});
+
+		socket.on("onDataChange", (data) => {
+			setValidatorData(data);
+		});
+	}, [])
 
 	return (
 		<AmplitudeProvider
