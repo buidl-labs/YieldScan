da5a37bbd401afc1d1004bad76e54053526bad5f
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-01-15 21:11:02 +0530

    make validator specifc view, view able in mobile
    still its not optimized for tablet view

--- src/components/validator_components/ValidatorApp.jsx
+++ src/components/validator_components/ValidatorApp.jsx
@@ -33,6 +33,7 @@ import {Helmet} from "react-helmet";
 import LogEvent from '../LogEvent';
 import axios from 'axios';
 import ErrorMessage from '../ErrorMessage';
+import Media from 'react-media';
 
 const ERA_PER_DAY = 4;
 class ValidatorApp extends React.Component {
@@ -170,39 +171,11 @@ class ValidatorApp extends React.Component {
 	};
 
 	render() {
-		const width = window.innerWidth - 400;
+		const width = window.innerWidth + 120;
+		const desktopWidth = window.innerWidth - 400;
 		const height = window.innerHeight - (64 + 69 + 50);
 		let radius = 120;
 
-		// let value = "";
-		// let validator = "";
-		// let valinfo = "";
-		// let totalinfo = "";
-		// if (!this.props.validatorandintentionloading && !this.state.isloading) {
-		// 	totalinfo = this.props.valtotalinfo;
-		// 	this.totalvalue =
-		// 		this.pathArray[4] === "kusama"
-		// 			? this.state.validatorInfo.stakers.total / Math.pow(10, 12)
-		// 			: this.state.validatorInfo.stakers.total / Math.pow(10, 15);
-		// 	this.ownvalue =
-		// 		this.pathArray[4] === "kusama"
-		// 			? this.state.validatorInfo.stakers.own / Math.pow(10, 12)
-		// 			: this.state.validatorInfo.stakers.own / Math.pow(10, 15);
-		// 	// validator = this.state.validatorInfo.accountId;
-		// 	valinfo = value;
-		// 	if (
-		// 		this.props.intentions.includes(
-		// 			this.props.history.location.pathname.split("/")[3].toString()
-		// 		)
-		// 	) {
-		// 		this.totalvalue =
-		// 			this.state.validatorInfo.stakingLedger.total / 10 ** 12;
-		// 		this.ownvalue = this.state.validatorInfo.stakingLedger.total / 10 ** 12;
-		// 	}
-		// }
-
-		// let totalBonded = 0;
-		// totalBonded = this.totalvalue.toFixed(3) - this.ownvalue.toFixed(3);
 		if (this.state.errorState) {
 			return <ErrorMessage />;
 		}	  
@@ -285,7 +258,232 @@ class ValidatorApp extends React.Component {
 							Copied to your clipboard
 						</Text> */}
 					</Box>
-					<Grid templateColumns="1fr 2fr" gap={2} overflowX="hidden">
+					<Media queries={{ small: "(max-width: 500px)" }}>
+						{matches =>
+							matches.small ? (
+								<Grid templateColumns="1fr" gap={2} overflowX="hidden">
+										<Box
+											width={350}
+											height={580}
+											style={{
+												marginLeft: 20,
+												marginTop: 30,
+												boxShadow:
+													"0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+												borderRadius: "10px",
+												padding: "5px 10px",
+											}}
+										>
+											<Flex flexDirection="column" alignItems="center">
+												<Text
+													align="center"
+													mt={2}
+													fontSize="2xl"
+													fontWeight="semibold"
+													lineHeight="short"
+												>
+													Key Stats
+												</Text>
+											</Flex>
+											<Divider />
+											<Flex flexDirection="column" style={{ padding: "0 20px" }}>
+												<Text mt={2} fontSize="md" fontWeight="bold" lineHeight="short">
+													Stake amount
+												</Text>
+												<Text fontSize="md" color="gray.500">
+													(change input to see potential earnings)
+												</Text>
+												<InputGroup>
+													<Input
+														placeholder="Stake Amount"
+														variant="filled"
+														type="number"
+														min="0"
+														step="0.000000000001"
+														max="999999999999999"
+														value={this.state.stakeInput}
+														textAlign="center"
+														roundedLeft="2rem"
+														onChange={e => {
+															const stakeAmount = isNaN(parseFloat(e.target.value))
+																? 0
+																: parseFloat(e.target.value);
+															const {
+																totalStake,
+																poolReward
+															} = this.state.currentValidatorData;
+															const userStakeFraction =
+																stakeAmount / (stakeAmount + totalStake);
+															const dailyEarning =
+																userStakeFraction * poolReward * ERA_PER_DAY; //if pool reward is zero then what ?
+															this.setState({
+																stakeInput: stakeAmount,
+																dailyEarning: dailyEarning.toFixed(3)
+															});
+														}}
+													/>
+													<InputRightAddon
+														children="KSM"
+														backgroundColor="teal.500"
+														roundedRight="2rem"
+													/>
+												</InputGroup>
+												<Text fontWeight="bold" mt="3" fontSize="md">
+													Daily Earning
+												</Text>
+												<Text>
+													<span
+														style={{
+															textTransform: "uppercase",
+															fontWeight: "bold",
+															color: "#E50B7B"
+														}}
+													>
+														{this.state.dailyEarning} KSM
+													</span>
+												</Text>
+											</Flex>
+											<Divider />
+											<Flex flexDirection="column" style={{ padding: "0 20px" }}>
+												<Text fontWeight="bold">Commission</Text>
+												<Text>{this.state.currentValidatorData.commission}%</Text>
+											</Flex>
+											<Divider />
+											<Flex flexDirection="column" style={{ padding: "0 20px" }}>
+												<Text fontWeight="bold">
+													Backers{" "}
+													<span style={{ color: "#718096", fontSize: 12 }}>
+														(number of stakers)
+													</span>
+												</Text>
+												<Text>{this.state.backers}</Text>
+											</Flex>
+											<Divider />
+											<Flex flexDirection="column" style={{ padding: "0 20px" }}>
+												<Text fontWeight="bold">Amount of stake</Text>
+												<Text mt={3} fontSize="12px">
+													Total
+												</Text>
+												<Text style={{ color: "#E50B7B", fontWeight: "bold" }}>
+													{this.state.totalStaked} KSM
+												</Text>
+												<Text fontSize="12px">Staked by self</Text>
+												<Text fontWeight="bold">{this.state.stakedBySelf} KSM</Text>
+												<Text fontSize="12px">staked by others</Text>
+												<Text fontWeight="bold">{this.state.stakedByOther} KSM</Text>
+											</Flex>
+										</Box>
+										<Stage width={width} height={height} scaleX={0.7} scaleY={0.7}>
+											<Layer>
+												{/* Here n is number of white circles to draw
+											r is radius of the imaginary circle on which we have to draw white circles
+											x,y is center of imaginary circle
+										*/}
+
+												<WhiteCircles
+													colorMode={this.props.colorMode}
+													n={this.state.nominators.length}
+													r={radius}
+													x={width / 2 + 13 - 10}
+													y={height / 2 + 6}
+													nominators={this.state.nominators}
+													history={this.props.history}
+													totalinfo={this.state.totalinfo}
+													valinfo={""}
+												/>
+
+												{/* Arc used to create the semicircle on the right,
+											Rotation is used to rotate the arc drawn by 90 degrees in clockwise direction
+										*/}
+												<Arc
+													x={width - 2}
+													y={height / 2}
+													innerRadius={height / 2 - 25}
+													outerRadius={height / 2 - 24}
+													rotation={90}
+													angle={180}
+													stroke={
+														this.props.colorMode === "light" ? "#CBD5E0" : "#718096"
+													}
+													strokeWidth={4}
+												/>
+												<Circle
+													x={width - 348}
+													y={height - 50}
+													radius={10}
+													fill="#319795"
+												/>
+												<TextRK
+													x={width - 325}
+													y={height - 56}
+													text="Nominator"
+													fill={
+														this.props.colorMode === "light" ? "#1A202C" : "#718096"
+													}
+													fontSize={15}
+												/>
+												<Rect
+													x={width - 360}
+													y={height - 30}
+													width={26}
+													height={15}
+													fill={color}
+													cornerRadius={10}
+												/>
+												<TextRK
+													x={width - 325}
+													y={height - 30}
+													text="Validator"
+													fill={
+														this.props.colorMode === "light" ? "#1A202C" : "#718096"
+													}
+													fontSize={15}
+												/>
+												{/* Adding 6 to stating and ending y point and 24 to length of line
+											because the upper left corner of rectangle is at width/2,height/2
+											so mid point of rectangle becomes width/2+12,height/2+6
+										*/}
+												<Line
+													points={[
+														width / 2 - 10,
+														height / 2 + 6,
+														width - height / 2 + 23,
+														height / 2 + 6
+													]}
+													fill={
+														this.props.colorMode === "light" ? "#1A202C" : "#FFFFFF"
+													}
+													stroke={
+														this.props.colorMode === "light" ? "#1A202C" : "#FFFFFF"
+													}
+													opacity={opacity}
+												/>
+
+												<TextRK
+													x={width / 2 + 25}
+													y={height / 2 - 15}
+													text={this.state.name}
+													fill={
+														this.props.colorMode === "light" ? "#1A202C" : "#FFFFFF"
+													}
+													fontSize={15}
+												/>
+
+												<Rect
+													x={width / 2 - 10}
+													y={height / 2}
+													width={26}
+													height={12}
+													fill={color}
+													cornerRadius={10}
+													onMouseOver={this.handleOnMouseOver}
+													onMouseOut={this.handleOnMouseOut}
+												/>
+											</Layer>
+										</Stage>
+									</Grid>
+							) : (
+								<Grid templateColumns="1fr 2fr" gap={2} overflowX="hidden">
 						<Box
 							width={350}
 							height={580}
@@ -397,7 +595,7 @@ class ValidatorApp extends React.Component {
 								<Text fontWeight="bold">{this.state.stakedByOther} KSM</Text>
 							</Flex>
 						</Box>
-						<Stage width={width} height={height} draggable={true}>
+						<Stage width={desktopWidth} height={height} draggable={true}>
 							<Layer>
 								{/* Here n is number of white circles to draw
 		                	r is radius of the imaginary circle on which we have to draw white circles
@@ -408,7 +606,7 @@ class ValidatorApp extends React.Component {
 									colorMode={this.props.colorMode}
 									n={this.state.nominators.length}
 									r={radius}
-									x={width / 2 + 13 - 10}
+									x={desktopWidth / 2 + 13 - 10}
 									y={height / 2 + 6}
 									nominators={this.state.nominators}
 									history={this.props.history}
@@ -420,7 +618,7 @@ class ValidatorApp extends React.Component {
 		            		Rotation is used to rotate the arc drawn by 90 degrees in clockwise direction
 		       			*/}
 								<Arc
-									x={width - 2}
+									x={desktopWidth - 2}
 									y={height / 2}
 									innerRadius={height / 2 - 25}
 									outerRadius={height / 2 - 24}
@@ -432,13 +630,13 @@ class ValidatorApp extends React.Component {
 									strokeWidth={4}
 								/>
 								<Circle
-									x={width - 348}
+									x={desktopWidth - 348}
 									y={height - 50}
 									radius={10}
 									fill="#319795"
 								/>
 								<TextRK
-									x={width - 325}
+									x={desktopWidth - 325}
 									y={height - 56}
 									text="Nominator"
 									fill={
@@ -447,7 +645,7 @@ class ValidatorApp extends React.Component {
 									fontSize={15}
 								/>
 								<Rect
-									x={width - 360}
+									x={desktopWidth - 360}
 									y={height - 30}
 									width={26}
 									height={15}
@@ -455,7 +653,7 @@ class ValidatorApp extends React.Component {
 									cornerRadius={10}
 								/>
 								<TextRK
-									x={width - 325}
+									x={desktopWidth - 325}
 									y={height - 30}
 									text="Validator"
 									fill={
@@ -469,9 +667,9 @@ class ValidatorApp extends React.Component {
 		         		*/}
 								<Line
 									points={[
-										width / 2 - 10,
+										desktopWidth / 2 - 10,
 										height / 2 + 6,
-										width - height / 2 + 23,
+										desktopWidth - height / 2 + 23,
 										height / 2 + 6
 									]}
 									fill={
@@ -484,7 +682,7 @@ class ValidatorApp extends React.Component {
 								/>
 
 								<TextRK
-									x={width / 2 + 25}
+									x={desktopWidth / 2 + 25}
 									y={height / 2 - 15}
 									text={this.state.name}
 									fill={
@@ -494,7 +692,7 @@ class ValidatorApp extends React.Component {
 								/>
 
 								<Rect
-									x={width / 2 - 10}
+									x={desktopWidth / 2 - 10}
 									y={height / 2}
 									width={26}
 									height={12}
@@ -506,6 +704,9 @@ class ValidatorApp extends React.Component {
 							</Layer>
 						</Stage>
 					</Grid>
+							)
+						}
+					</Media>
 					{/* <Flex justifyContent="center">
 						<Box
 							mt={12}
