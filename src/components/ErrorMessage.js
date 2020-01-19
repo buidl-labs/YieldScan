415966b01cb37eda1190c68408424004279814bc
saumyakaran <saumya942@gmail.com>
2020-01-19 18:11:47 +0530

    Revert "update error message content"
    This reverts commit 407909c277006ee99e800e821512ec6be768c9eb.

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import {
-	Box,
-	Text,
-	Heading,
-	Link as ChakraLink,
-  Button
-} from "@chakra-ui/core";
+import { Link as ChakraLink } from '@chakra-ui/core';
+
 function ErrorMessage() {
   return (
     <div
@@ -16,29 +11,23 @@ function ErrorMessage() {
         height: 'calc(100vh - 40px)'
       }}
     >
-    <Box>
-      <div style={{textAlign:"center"}}>
-     <Heading as="h1" size="xl">
-				Error Loading The Page!
-			</Heading>
-      <Heading as="h3" size="l">
-				Try reloading the page
-			</Heading>
-      <Button style={{marginTop: 10}} onClick={() => {
-        window.location.reload();
-      }}>Retry</Button>
-      </div>
-			<Text color="gray.500" mt={8}>
-				please report it to{" "}
-				<ChakraLink
-					href="mailto:bhaskar@thevantageproject.com"
-					color="teal.500"
-				>
-					bhaskar@thevantageproject.com
-				</ChakraLink>
-				, we will reach out to you as soon as possible
-			</Text>
-		</Box>
+      <p
+        style={{
+          fontSize: '30px',
+          fontWeight: 'bold',
+          textAlign: 'center',
+          margin: 70
+        }}
+      >
+        We are down right now, report it here{' '}
+        <ChakraLink
+          href="mailto:bhaskar@thevantageproject.com"
+          color="teal.500"
+        >
+          bhaskar@thevantageproject.com
+        </ChakraLink>{' '}
+        we will be back soon.
+      </p>
     </div>
   );
 }
