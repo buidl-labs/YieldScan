f72b8b3d21cfa9284ecb631464bccbd8ce8f1c54
Bhaskar Singh <allstarbhaskarkumar@gmail.com>
2020-02-07 18:24:32 +0530

    Add login/sign up logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,142 +1,288 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import {
-	Flex,
-	useDisclosure,
-	Box,
-	Image,
-	Heading,
-	Link,
-	IconButton,
-	useColorMode,
-	Button,
-	Drawer,
-	DrawerOverlay,
-	DrawerContent,
-	DrawerCloseButton,
-	DrawerHeader,
-	DrawerBody,
-	DrawerFooter,
-	Text,
-	Icon
-} from "@chakra-ui/core";
-import { NavLink } from "react-router-dom";
-import { AiOutlineMenu } from "react-icons/ai";
+  Flex,
+  useDisclosure,
+  Box,
+  Image,
+  Heading,
+  Link,
+  IconButton,
+  useColorMode,
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  Text,
+  Icon,
+  MenuItem,
+  MenuButton,
+  Menu,
+  MenuList
+} from '@chakra-ui/core';
+import { NavLink } from 'react-router-dom';
+import { AiOutlineMenu } from 'react-icons/ai';
+import {
+  web3Accounts,
+  web3Enable,
+  web3FromAddress,
+  isWeb3Injected,
+  web3AccountsSubscribe
+} from '@polkadot/extension-dapp';
+
+import Identicon from '@polkadot/react-identicon';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import AlertDialogContainer from './LoginFlow/AlertDialogContainer';
 
 export default () => {
-	const { colorMode, toggleColorMode } = useColorMode();
-	const { isOpen, onOpen, onClose } = useDisclosure();
-	const btnRef = React.useRef();
-	return (
-		<React.Fragment>
-			{/* Navbar */}
-			<Flex direction="row" justifyContent="space-between" zIndex={999} p={2}>
-				{/* Polka Analytics Logo - Left hand part of navbar */}
-				<Flex justify="flex-start" alignItems="center">
-					<NavLink to="/">
-						<Box as="span" display="inline-flex" alignItems="center">
-							<Image src="/logo192.png" height="2rem" mr={4} />
-							<Heading
-								as="h3"
-								size="lg"
-								display={{ base: "none", sm: "block" }}
-							>
-								Polka Analytics
-							</Heading>
-						</Box>
-					</NavLink>
-				</Flex>
-				{/* Navigation Menu & color mode toggle - Right hand part of navbar */}
-				<Flex justify="flex-end">
-					<Flex alignItems="center" display={{ base: "none", sm: "flex" }}>
-						<Box mr={8}>
-							<Link as={NavLink} className="nav-link" to="/dashboard">
-								Dashboard
-							</Link>
-						</Box>
-						<Box mr={8}>
-							<Link as={NavLink} className="nav-link" to="/help-center">
-								Help Center
-							</Link>
-						</Box>
-						<IconButton
-							aria-label={
-								colorMode === "light"
-									? "Switch to dark mode"
-									: "Switch to light mode"
-							}
-							icon={colorMode === "light" ? "moon" : "sun"}
-							size="lg"
-							onClick={toggleColorMode}
-							backgroundColor={colorMode === "light" ? "#fff" : "gray.800"}
-						/>
-					</Flex>
-					<IconButton
-						ref={btnRef}
-						icon={AiOutlineMenu}
-						onClick={onOpen}
-						display={{ base: "block", sm: "none" }}
-						backgroundColor="transparent"
-					/>
-					<Drawer
-						isOpen={isOpen}
-						placement="right"
-						onClose={onClose}
-						finalFocusRef={btnRef}
-						display={{ base: "block", sm: "none" }}
-					>
-						<DrawerOverlay />
-						<DrawerContent>
-							<DrawerCloseButton />
-							<DrawerHeader>Polka Analytics</DrawerHeader>
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isExtensionDialogOpen,
+    onOpen: onExtensionDialogOpen,
+    onClose: onExtensionDialogClose
+  } = useDisclosure();
+  const {
+    isOpen: isCreateAccountDialogOpen,
+    onOpen: onCreateAccountDialogOpen,
+    onClose: onCreateAccountDialogClose
+  } = useDisclosure();
+  const [isUserLoggedIn, setIsLoggedIn] = useState(false);
+  const [listOfUsers, updateListOfUsers] = useState([]);
 
-							<DrawerBody>
-								<Box mr={8}>
-									<Link
-										as={NavLink}
-										className="nav-link"
-										to="/dashboard"
-										py={4}
-									>
-										Dashboard
-									</Link>
-								</Box>
-								<Box mr={8}>
-									<Link
-										as={NavLink}
-										className="nav-link"
-										to="/help-center"
-										py={4}
-									>
-										Help Center
-									</Link>
-								</Box>
-							</DrawerBody>
+  const btnRef = React.useRef();
 
-							<DrawerFooter
-								justifyContent="flex-start"
-								alignItems="center"
-								p={0}
-							>
-								<Button onClick={toggleColorMode} pl={6} width="100%">
-									<Icon
-										size="16px"
-										aria-label={
-											colorMode === "light"
-												? "Switch to dark mode"
-												: "Switch to light mode"
-										}
-										name={colorMode === "light" ? "moon" : "sun"}
-										mr={4}
-									/>{" "}
-									{colorMode === "light"
-										? "Switch to dark mode"
-										: "Switch to light mode"}
-								</Button>
-							</DrawerFooter>
-						</DrawerContent>
-					</Drawer>
-				</Flex>
-			</Flex>
-		</React.Fragment>
-	);
+  //check if user has at least one account with polkadot extension enabled
+  useEffect(() => {
+    // console.log('isWeb3Injected', isWeb3Injected);
+    // connected to polkadot extension
+    if (isWeb3Injected) {
+      //connect to extension
+      web3Enable('Polkanalytics');
+
+      //subscriber to listen to change in extension accounts
+      web3AccountsSubscribe(users => {
+        console.log('values', users);
+        if (users.length > 0) {
+          /*
+            Means user has at least setup on account,
+            and store account info in localstorage for global reference
+          */
+          setIsLoggedIn(true);
+          updateListOfUsers(users);
+          localStorage.setItem('users', JSON.stringify(users));
+        } else {
+          //user has no accounts setup/created
+          updateListOfUsers(users);
+          setIsLoggedIn(false);
+          localStorage.setItem('users', JSON.stringify(users));
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      {/* Navbar */}
+      <Flex direction="row" justifyContent="space-between" zIndex={999} p={2}>
+        {/* Polka Analytics Logo - Left hand part of navbar */}
+        <Flex justify="flex-start" alignItems="center">
+          <NavLink to="/">
+            <Box as="span" display="inline-flex" alignItems="center">
+              <Image src="/logo192.png" height="2rem" mr={4} />
+              <Heading
+                as="h3"
+                size="lg"
+                display={{ base: 'none', sm: 'block' }}
+              >
+                Polka Analytics
+              </Heading>
+            </Box>
+          </NavLink>
+        </Flex>
+        {/* Navigation Menu & color mode toggle - Right hand part of navbar */}
+        <Flex justify="flex-end">
+          <Flex alignItems="center" display={{ base: 'none', sm: 'flex' }}>
+            {isUserLoggedIn ? (
+              <Box mr={8}>
+                <Menu>
+                  <MenuButton as={Button} rightIcon="chevron-down">
+                    Accounts
+                  </MenuButton>
+                  <MenuList style={{ zIndex: 1000, overflowY: 'auto' }}>
+                    {listOfUsers.length > 0 &&
+                      listOfUsers.map(user => {
+                        return (
+                          <MenuItem minH="48px">
+                            <Identicon
+                              value={user.address}
+                              size={36}
+                              theme="polkadot"
+                            />
+                            <strong style={{ marginLeft: 10 }}>
+                              {user.meta.name}
+                            </strong>
+                          </MenuItem>
+                        );
+                      })}
+                  </MenuList>
+                </Menu>
+              </Box>
+            ) : (
+              <Box mr={8}>
+                <Button
+                  style={{
+                    border: '1px solid #E50B7B',
+                    borderRadius: '18px',
+                    opacity: 1,
+                    color: '#E50B7B',
+                    height: '35px'
+                  }}
+                  onClick={async () => {
+                    //if yes continue
+                    //Check if extension is available
+                    if (!isWeb3Injected) {
+                      //if not: show a dialog box with link to add extension
+                      //link to extension: https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd?hl=en
+                      //return and not continue with the code execution
+                      onExtensionDialogOpen();
+                      console.log('add polkadot extensions');
+                      return;
+                    }
+
+                    if (listOfUsers.length <= 0) {
+                      onCreateAccountDialogOpen();
+                    }
+                  }}
+                >
+                  Sign in / Sign up
+                </Button>
+              </Box>
+            )}
+            <Box mr={8}>
+              <Link as={NavLink} className="nav-link" to="/dashboard">
+                Dashboard
+              </Link>
+            </Box>
+            <Box mr={8}>
+              <Link as={NavLink} className="nav-link" to="/help-center">
+                Help Center
+              </Link>
+            </Box>
+            <IconButton
+              aria-label={
+                colorMode === 'light'
+                  ? 'Switch to dark mode'
+                  : 'Switch to light mode'
+              }
+              icon={colorMode === 'light' ? 'moon' : 'sun'}
+              size="lg"
+              onClick={toggleColorMode}
+              backgroundColor={colorMode === 'light' ? '#fff' : 'gray.800'}
+            />
+          </Flex>
+          <IconButton
+            ref={btnRef}
+            icon={AiOutlineMenu}
+            onClick={onOpen}
+            display={{ base: 'block', sm: 'none' }}
+            backgroundColor="transparent"
+          />
+          <Drawer
+            isOpen={isOpen}
+            placement="right"
+            onClose={onClose}
+            finalFocusRef={btnRef}
+            display={{ base: 'block', sm: 'none' }}
+          >
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>Polka Analytics</DrawerHeader>
+
+              <DrawerBody>
+                <Box mr={8}>
+                  <Link
+                    as={NavLink}
+                    className="nav-link"
+                    to="/dashboard"
+                    py={4}
+                  >
+                    Dashboard
+                  </Link>
+                </Box>
+                <Box mr={8}>
+                  <Link
+                    as={NavLink}
+                    className="nav-link"
+                    to="/help-center"
+                    py={4}
+                  >
+                    Help Center
+                  </Link>
+                </Box>
+              </DrawerBody>
+
+              <DrawerFooter
+                justifyContent="flex-start"
+                alignItems="center"
+                p={0}
+              >
+                <Button onClick={toggleColorMode} pl={6} width="100%">
+                  <Icon
+                    size="16px"
+                    aria-label={
+                      colorMode === 'light'
+                        ? 'Switch to dark mode'
+                        : 'Switch to light mode'
+                    }
+                    name={colorMode === 'light' ? 'moon' : 'sun'}
+                    mr={4}
+                  />{' '}
+                  {colorMode === 'light'
+                    ? 'Switch to dark mode'
+                    : 'Switch to light mode'}
+                </Button>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </Flex>
+      </Flex>
+      <AlertDialogContainer
+        isOpen={isExtensionDialogOpen}
+        onClose={onExtensionDialogClose}
+        title="Polkadot JS Extension Required!"
+        body={
+          <>
+            PolkadotJs extension allows you to manage your polkadot accounts
+            outside of dapps. Injects the accounts and allows signs transactions
+            for a specific account.
+            <div>
+              <Link
+                href="https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd?hl=en"
+                isExternal
+                color="teal.500"
+              >
+                Add PolkadotJs Extension
+              </Link>
+            </div>
+          </>
+        }
+      />
+      <AlertDialogContainer
+        isOpen={isCreateAccountDialogOpen}
+        onClose={onCreateAccountDialogClose}
+        title="Create atleast one account from polkadot extension!"
+        body={
+          <>
+            Create atleast one account from PolkadotJs extension for making
+            transactions for a specific account.
+          </>
+        }
+      />
+    </React.Fragment>
+  );
 };
